cb1aace41dcc0d5d625f03f1a6c318de5f61dead
myloo23 <milan.takacs24@gmail.com>
2026-03-26 09:06:07 +0100

    Add event fragment navi es kepernyo

--- app/src/main/java/com/example/kifizeti_android/MainActivity.java
+++ app/src/main/java/com/example/kifizeti_android/MainActivity.java
@@ -2,23 +2,21 @@ package com.example.kifizeti_android;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.kifizeti_android.ui.add.AddEventFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // induláskor betöltjük az AddEventFragmentet
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new AddEventFragment())
+                .commit();
     }
 }
